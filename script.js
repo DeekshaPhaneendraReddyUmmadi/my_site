0070b1013e384121ee06dd3662064f427fae5532
phani <deekshaphaneendrareddy@gmail.com>
2023-02-16 07:34:48 -0500

    ADD index.html,style.css,script.js files and images

---
+++ script.js
@@ -0,0 +1,6 @@
+for(let i=0;i<3;i++)
+{
+    document.querySelectorAll(".r1")[i].addEventListener("click",function(){
+        alert("This link is not gona work! Use Contact me instead.");
+    })
+}
